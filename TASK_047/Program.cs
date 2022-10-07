e2bdba7f18afc40c3f173dc2f6f7f1ef48b1405a
Alina Lavrik <alina_voro@mail.ru>
2022-10-07 18:46:57 +0300

    Add Task 47

---
+++ TASK_047/Program.cs
@@ -0,0 +1,40 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+m = 3, n = 4.
+
+1 7 -2 -85
+1 -3 8 -9
+8 7 -7 9 */
+
+
+Console.Write("Количество строк: ");
+int rows = int.Parse(Console.ReadLine()!);
+
+Console.Write("Количество столбцов: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, -99, 100);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i,j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
